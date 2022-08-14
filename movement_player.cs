9dbfe565b876fb2ad3d877b21dc265ac6190ce31
ManiBunny <110626500+ManiBunny@users.noreply.github.com>
2022-08-14 21:18:05 +0300

    Initial commit

---
+++ movement_player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class movement_player : MonoBehaviour
+{
+    private bool isGrounded= true;
+    private Rigidbody2D rb;
+    float horizontal;
+    public float moveSpeed = 15f;
+    public float jumpAmount = 10;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "ground" && isGrounded == false)
+        {
+            isGrounded = true;
+        }
+    }
+}
